49ad8ff6ab6937790cd72f83c40122586beb93f7
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 20:35:53 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть початок діапазону: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть кінець діапазону: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        if (start > end)
+        {
+            Console.WriteLine("Помилка: початок діапазону не може бути більше кінця. Виконуємо нормалізацію.");
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+
+        Console.WriteLine($"Парні числа в діапазоні від {start} до {end}:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
