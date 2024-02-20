409bc3fe1f36908e4d44c97253bda404fafd1a26
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 20:25:24 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть перше число: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть друге число (відсоток): ");
+        double percent = Convert.ToDouble(Console.ReadLine());
+
+        double result = (percent / 100) * number1;
+
+        Console.WriteLine($"Результат: {percent}% від {number1} = {result}");
+    }
+}
