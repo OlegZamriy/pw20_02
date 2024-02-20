e45c21107505fa6b19ee63e5dbf75838b726b549
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 19:44:02 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть шестизначне число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine($"Результат перевертання числа: {reversedNumber}");
+    }
+
+    static int ReverseNumber(int num)
+    {
+        int reversedNum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        return reversedNum;
+    }
+}
