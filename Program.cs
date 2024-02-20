8112c6938604f8de67c5d59f1499f125b842631e
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 19:38:20 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string quote = "It's easy to win forgiveness for being wrong; being right is what gets you into real trouble.";
+        string author = "Bjarne Stroustrup";
+
+        Console.WriteLine($"{quote}\n{author}");
+    }
+}
