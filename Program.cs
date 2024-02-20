1449942cf43852bb6f1929da2f45132bafab0cbb
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 19:49:42 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть довжину лінії: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть символ заповнювач: ");
+        char fillChar = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Введіть напрямок лінії (h - горизонтальна, v - вертикальна): ");
+        char direction = Convert.ToChar(Console.ReadLine());
+
+        if (direction == 'h')
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(fillChar);
+            }
+        }
+        else if (direction == 'v')
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(fillChar);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Помилка: неправильно введений напрямок лінії.");
+        }
+    }
+}
