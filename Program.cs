94082085ea55aec16cdb299a3f411c8a9321f6ce
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 20:24:31 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число від 1 до 100: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 1 || number > 100)
+        {
+            Console.WriteLine("Помилка: число має бути від 1 до 100.");
+            return;
+        }
+
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (number % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
