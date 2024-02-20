012fba2d16fcfb079b89897dbb49cce1daf5bf8e
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 19:48:19 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть значення A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть значення B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        if (A >= B)
+        {
+            Console.WriteLine("Помилка: A повинне бути менше B.");
+            return;
+        }
+
+        for (int i = A; i <= B; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
