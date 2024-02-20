8558803e8889a345d41fef42f66a7df801ce29ca
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-20 20:26:09 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть чотири цифри:");
+
+        int[] digits = new int[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            Console.Write($"Цифра {i + 1}: ");
+            digits[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int number = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
+
+        Console.WriteLine($"Отримане число: {number}");
+    }
+}
